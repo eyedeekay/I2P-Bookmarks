e270b2bf7cb88314f4ac336ad1d6f98701d1249b
idk <hankhill19580@gmail.com>
2022-10-19 18:03:36 -0400

    add some basic I2P bookmarks

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-TASSEL_NAME="tassel"
-TASSEL_DESC="Template project for adding bookmarks to a web browser with an extension"
+TASSEL_NAME="I2PBookmarks"
+TASSEL_DESC="Automatically adds I2P Bookmarks to any browser supporting WebExtensions"
 TASSEL_VERSION=0.0.1
-TASSEL_HOME="https://github.com/eyedeekay/Tassel"
+TASSEL_HOME="https://github.com/eyedeekay/I2P-Bookmarks"
 TASSEL_HOST="eyedeekay.github.io"
